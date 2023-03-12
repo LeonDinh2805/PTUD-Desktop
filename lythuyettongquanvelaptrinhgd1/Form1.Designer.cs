15d8b6ad22244bb71736b15c0051e59494f4dc35
LeonDinh <93775704+LeonDinh2805@users.noreply.github.com>
2023-03-12 21:55:02 +0700

    Add files via upload

---
+++ lythuyettongquanvelaptrinhgd1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace lythuyettongquanvelaptrinhgd1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupPanelTabcontrolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ptb = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.groupPanelTabcontrolToolStripMenuItem,
+            this.ptb,
+            this.menu});
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(191, 20);
+            this.ToolStripMenuItem.Text = "Đinh Lê Quang Nguyên 2111867 ";
+            // 
+            // groupPanelTabcontrolToolStripMenuItem
+            // 
+            this.groupPanelTabcontrolToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("groupPanelTabcontrolToolStripMenuItem.Image")));
+            this.groupPanelTabcontrolToolStripMenuItem.Name = "groupPanelTabcontrolToolStripMenuItem";
+            this.groupPanelTabcontrolToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.groupPanelTabcontrolToolStripMenuItem.Text = "group, panel ";
+            this.groupPanelTabcontrolToolStripMenuItem.Click += new System.EventHandler(this.groupPanelTabcontrolToolStripMenuItem_Click);
+            // 
+            // ptb
+            // 
+            this.ptb.Image = ((System.Drawing.Image)(resources.GetObject("ptb.Image")));
+            this.ptb.Name = "ptb";
+            this.ptb.Size = new System.Drawing.Size(180, 22);
+            this.ptb.Text = " imagelist  ";
+            this.ptb.Click += new System.EventHandler(this.ptb_Click);
+            // 
+            // menu
+            // 
+            this.menu.Image = ((System.Drawing.Image)(resources.GetObject("menu.Image")));
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(180, 22);
+            this.menu.Text = "menustrip";
+            this.menu.Click += new System.EventHandler(this.menu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem groupPanelTabcontrolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ptb;
+        private System.Windows.Forms.ToolStripMenuItem menu;
+    }
+}
+
